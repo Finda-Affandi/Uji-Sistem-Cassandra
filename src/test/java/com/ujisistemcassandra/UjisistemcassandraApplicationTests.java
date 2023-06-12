e300f317d37639ad7a89b847ad4cc442eb1109cf
fann <f.affnd@gmail.com>
2023-06-12 22:25:58 +0700

    Initial commit

---
+++ src/test/java/com/ujisistemcassandra/UjisistemcassandraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ujisistemcassandra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UjisistemcassandraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
