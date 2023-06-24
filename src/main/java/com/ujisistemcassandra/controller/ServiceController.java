5103763d681bbe3aa7a175ac5e2e4188cc5b4aeb
fann <f.affnd@gmail.com>
2023-06-24 12:28:24 +0700

    Change model from entity into Map Object

--- src/main/java/com/ujisistemcassandra/controller/ServiceController.java
+++ src/main/java/com/ujisistemcassandra/controller/ServiceController.java
@@ -1,34 +1,50 @@
 package com.ujisistemcassandra.controller;
 
 
-import com.ujisistemcassandra.entity.Mahasiswa;
-import com.ujisistemcassandra.repository.MahasiswaRepository;
+import com.ujisistemcassandra.repository.ServiceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/api")
 public class ServiceController {
 
-    private final MahasiswaRepository mahasiswaRepository;
+    private final ServiceRepository serviceRepository;
     @Autowired
-    public ServiceController(MahasiswaRepository mahasiswaRepository) {
-        this.mahasiswaRepository = mahasiswaRepository;
+    public ServiceController(ServiceRepository serviceRepository) {
+        this.serviceRepository = serviceRepository;
     }
 
-    @GetMapping("/mahasiswa")
-    public List<Mahasiswa> getAllMahasiswa() {
-        return mahasiswaRepository.findAll();
+    @GetMapping("/cassandra")
+    public ResponseEntity<List<Map<String, Object>>> getAllData() {
+        try {
+            List<Map<String, Object>> dataList = serviceRepository.getAllData();
+            return ResponseEntity.ok(dataList);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
-    @PostMapping("/mahasiswa")
-    public ResponseEntity<Void> saveMahasiswa(@RequestBody Mahasiswa mahasiswa) {
-        mahasiswaRepository.saveMahasiswa(mahasiswa);
-        return ResponseEntity.ok().build();
+    @PostMapping("/cassandra")
+    public ResponseEntity<String> insertData(@RequestBody List<Map<String, Object>> dataList) {
+        try {
+            for (Map<String, Object> data : dataList) {
+            serviceRepository.insertData(data);
+                System.out.println(data.keySet());
+                System.out.println(data.values());
+            }
+            return ResponseEntity.ok("Data inserted succesfully!");
+        } catch (Exception e) {
+            String eMessage = "Faied to insert data!";
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(eMessage);
+        }
     }
 
 
